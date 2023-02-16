fe4db7c807406c605ac551cc5c2548e46364ec99
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-16 19:23:34 +0300

    MainPage and AdminPage

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from '../Components/NavBar/NavBar';
+import MainPage from '../Components/Main/MainPage';
 
 function App(): JSX.Element {
   return (
     <div>
       <Routes>
         <Route path="/" element={<NavBar />}>
-          {/* <Route index element={<MainPage />} /> */}
+          <Route index element={<MainPage />} />
           {/* <Route path="/cases" element={<CasesPage />} /> */}
           {/* <Route path="/news" element={<NewsPage />} /> */}
         </Route>
--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -4,41 +4,40 @@ import { NavLink, Outlet } from 'react-router-dom';
 
 export default function NavBar(): JSX.Element {
   const user = {
-    id: 1,
+    idi: 1,
   };
   return (
-    <nav className="flex flex-col justify-center items-center py-3 bg-black text-white shadow-lg navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-        <div className="flex-1 flex justify-between mr-auto">
-          <NavLink className="text-xl text-white" to="/">
-            <img src="/img/2023-02-16 16.27.36.jpg" style={{ height: '30px' }} alt="" />
-          </NavLink>
-          {'id' in user ? (
-            <div
-              className="flex space-evenly "
-              style={{ width: '25%', justifyContent: 'space-between' }}
-            >
-              <div>Admin Panel</div>
-            </div>
-          ) : (
-            <div
-              className="flex space-evenly "
-              style={{ width: '25%', justifyContent: 'space-between' }}
-            >
-              <NavLink className="text-xl text-white" to="/about">
-                О нас
-              </NavLink>
-              <NavLink className="text-xl text-white" to="/cases">
-                Кейсы
-              </NavLink>
-              <NavLink className="text-xl text-white" to="/news">
-                Новости
-              </NavLink>
-            </div>
-          )}
+    <>
+      <nav className="flex flex-col justify-center items-center py-3 bg-black text-white shadow-lg navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+          <div className="flex-1 flex justify-between mr-auto">
+            <NavLink className="text-xl text-white" to="/">
+              <img src="/img/2023-02-16 16.27.36.jpg" style={{ height: '30px' }} alt="" />
+            </NavLink>
+            {'id' in user ? (
+              <div className="flex space-evenly ">
+                <div style={{ fontSize: '20px', fontWeight: 'bold' }}>ADMIN PANEL</div>
+              </div>
+            ) : (
+              <div
+                className="flex space-evenly "
+                style={{ width: '25%', justifyContent: 'space-between' }}
+              >
+                <NavLink className="text-xl text-white" to="/about">
+                  О нас
+                </NavLink>
+                <NavLink className="text-xl text-white" to="/cases">
+                  Кейсы
+                </NavLink>
+                <NavLink className="text-xl text-white" to="/news">
+                  Новости
+                </NavLink>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </nav>
       <Outlet />
-    </nav>
+    </>
   );
 }
