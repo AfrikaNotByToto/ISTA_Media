d7434c1e44f6c7f323688a808832ae79a549647d
eddy49reg <akhnazarov1990@gmail.com>
2023-02-16 19:32:50 +0300

    create postSlice

---
+++ frontend/src/PostSlice.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import * as api from './App/api';
+
+import { Post, State } from './Components/Posts/Types/types';
