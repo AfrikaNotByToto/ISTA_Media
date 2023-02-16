1400e4a19629024e1eae7110449d6d86f00dc553
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-16 14:40:42 +0300

    Start frontend and tailwind

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './App.css';
+
+function App(): JSX.Element {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
