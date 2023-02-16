7105293a10831f44bb6c3c6dad284c0d67a4ba13
eddy49reg <akhnazarov1990@gmail.com>
2023-02-16 18:29:35 +0300

    connect Post routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const db = require('./db/models');
-const config = require('./config/config');
+const db = require("./db/models");
+const config = require("./config/config");
 
-const apiAuthRoute = require('./routes/auth.routes');
+const apiAuthRoute = require("./routes/auth.routes");
+const apiPostRoute = require("./routes/posts.routes");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 config(app);
 
-app.use('/api/auth', apiAuthRoute);
+app.use("/api/auth", apiAuthRoute);
+app.use("/api/posts", apiPostRoute);
 
 const start = async () => {
   try {
