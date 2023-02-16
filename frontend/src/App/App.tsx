9019687170401cc24d57bb5d32c24e68015d9059
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-16 16:58:02 +0300

    NavBar and Routes

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from '../Components/NavBar/NavBar';
 
 function App(): JSX.Element {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          {/* <Route index element={<MainPage />} /> */}
+          {/* <Route path="/cases" element={<CasesPage />} /> */}
+          {/* <Route path="/news" element={<NewsPage />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
