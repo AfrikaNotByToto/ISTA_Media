afb4609c75dadaa65e548357db16ffd47822fb00
Tanya Shmatchenko <117009692+AfrikaNotByToto@users.noreply.github.com>
2023-02-18 15:01:55 +0300

    new photo on main

--- frontend/src/Components/Main/MainPage.tsx
+++ frontend/src/Components/Main/MainPage.tsx
@@ -11,25 +11,39 @@ export default function MainPage(): JSX.Element {
   return (
     <>
       {user && (
-        <div>
-          <div style={{ marginTop: '150px', marginLeft: '50px' }}>
-            <h1
-              className="h11"
-              style={{ color: 'whitesmoke', fontSize: '50px', fontWeight: 'bolder' }}
-            >
-              Сделаем ваш бизнес узнаваемым
-            </h1>
-            <h5 className="h55" style={{ color: 'whitesmoke', fontSize: '30px' }}>
-              сотрудничаем с застройщиками,риэлторскими агенствами и отелями
-            </h5>
-          </div>
-          <div className="flex justify-end">
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/5d0ade01fc40e30001b4a6c5/1611179474887-THCHA3NFBUUQD9DGLOSV/bw+spiral+gif.gif"
-              alt=""
-              style={{ height: '100%' }}
-            />
-          </div>
+        <div className="container" style={{ position: 'relative' }}>
+          <img
+            src="https://99px.ru/sstorage/53/2013/04/tmb_68339_3636.jpg"
+            alt=""
+            style={{
+              height: '100%',
+              width: '100%',
+            }}
+          />
+          <h1
+            className="h11"
+            style={{
+              position: 'absolute',
+              top: '40%',
+              left: '10%',
+              color: 'whitesmoke',
+              fontSize: '50px',
+              fontWeight: 'bolder',
+            }}
+          >
+            Сделаем ваш бизнес узнаваемым
+          </h1>
+          <h5
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '10%',
+              color: 'whitesmoke',
+              fontSize: '30px',
+            }}
+          >
+            сотрудничаем с застройщиками,риэлторскими агенствами и отелями
+          </h5>
         </div>
       )}
 
