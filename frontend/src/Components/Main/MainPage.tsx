58789608428530f1eb184c08536b7d15f992c665
Tanya Shmatchenko <117009692+AfrikaNotByToto@users.noreply.github.com>
2023-02-19 11:32:27 +0300

    changed background color

--- frontend/src/Components/Main/MainPage.tsx
+++ frontend/src/Components/Main/MainPage.tsx
@@ -7,41 +7,52 @@ import Request from '../Requests/Request';
 export default function MainPage(): JSX.Element {
   return (
     <>
-        <div className="container" style={{ position: 'relative' }}>
-          <img
-            className="bg-contain bg-center"
-            src="https://99px.ru/sstorage/53/2013/04/tmb_68339_3636.jpg"
-            alt=""
-            style={{
-              height: '200vh',
-              width: '400vw',
-            }}
-          />
-          <h1
-            className="h11"
-            style={{
-              position: 'absolute',
-              top: '15%',
-              left: '5%',
-              color: 'whitesmoke',
-              fontSize: '60px',
-              fontWeight: 'bolder',
-            }}
-          >
-            Ваши клиенты вас найдут
-          </h1>
-          <h5
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '10%',
-              color: 'whitesmoke',
-              fontSize: '30px',
-            }}
-          >
-            сотрудничаем с застройщиками,риэлторскими агенствами и отелями
-          </h5>
-        </div>
+      <div className="container" style={{ position: 'relative' }}>
+        <img
+          className="bg-contain bg-center"
+          src="https://99px.ru/sstorage/53/2013/04/tmb_68339_3636.jpg"
+          alt=""
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+        />
+        <h1
+          className="h11"
+          style={{
+            position: 'absolute',
+            top: '15%',
+            left: '5%',
+            color: 'whitesmoke',
+            fontSize: '60px',
+            fontWeight: 'bolder',
+          }}
+        >
+          Ваши клиенты вас найдут
+        </h1>
+        <h5
+          style={{
+            position: 'absolute',
+            top: '30%',
+            left: '5%',
+            color: 'whitesmoke',
+            fontSize: '30px',
+          }}
+        >
+          сотрудничаем с застройщиками,
+        </h5>
+        <h5
+          style={{
+            position: 'absolute',
+            top: '38%',
+            left: '5%',
+            color: 'whitesmoke',
+            fontSize: '30px',
+          }}
+        >
+          риэлторскими агенствами и отелями
+        </h5>
+      </div>
       <div
         className="flex"
         style={{
