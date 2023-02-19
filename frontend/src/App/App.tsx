5cf8bf643caa2be99dbfacc480e4f36592837534
Tanya Shmatchenko <117009692+AfrikaNotByToto@users.noreply.github.com>
2023-02-19 12:01:12 +0300

    trying modal window

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -10,10 +10,12 @@ import MainPage from '../Components/Main/MainPage';
 import AdminPage from '../Components/Admin/AdminPage';
 import Registration from '../Components/auth/RegistrationForm';
 import Authorization from '../Components/auth/AuthForm';
+import Modal from '../Components/Modal/Modal';
 
 function App(): JSX.Element {
   return (
     <div>
+      <Modal />
       <Routes>
         <Route path="/" element={<NavBar />}>
           {/* <Route index element={<MainPage />} /> */}
