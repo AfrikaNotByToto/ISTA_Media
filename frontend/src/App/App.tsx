193a902e8d330e024efbbb189640cab61de05903
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-20 13:21:01 +0300

    CheckUserAndLogout

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
-import './App.css';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
 import NavBar from '../Components/NavBar/NavBar';
 
 import PostPage from '../Components/Posts/PostPage';
@@ -10,8 +10,15 @@ import MainPage from '../Components/Main/MainPage';
 import AdminPage from '../Components/Admin/AdminPage';
 import Registration from '../Components/auth/RegistrationForm';
 import Authorization from '../Components/auth/AuthForm';
+import { useAppDispatch } from '../store';
+import { checkUser } from '../Components/auth/authRegSlice';
 
 function App(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(checkUser());
+  }, []);
   return (
     <div>
       <Routes>
