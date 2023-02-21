9248174e9ab1334bf02d4e77bfdb458014ebdc86
eddy49reg <akhnazarov1990@gmail.com>
2023-02-21 14:42:09 +0300

    delete consolelogs

--- backend/routes/phoneForm.routes.js
+++ backend/routes/phoneForm.routes.js
@@ -4,7 +4,6 @@ const { PhoneForm } = require('../db/models');
 router.post('/', async (req, res) => {
   try {
     const { phone } = req.body;
-    console.log(phone);
     let isTrue = await PhoneForm.findOne({ where: { phone: phone } });
     if (phone === null || isTrue === null) {
       const newPhone = await PhoneForm.create({
@@ -12,7 +11,7 @@ router.post('/', async (req, res) => {
       });
       res.status(200).json(newPhone);
     } else {
-      res.status(300), json({ message: 'Такой номер уже существует' });
+      res.status(406), json({ message: 'Такой номер уже существует' });
     }
   } catch (message) {
     res.status(500).json({ message: 'Crushed' });
