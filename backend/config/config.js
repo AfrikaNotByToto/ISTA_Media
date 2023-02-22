617023ac8f1e7716a64d040cb4b74c633de8855f
Tanya Shmatchenko <117009692+AfrikaNotByToto@users.noreply.github.com>
2023-02-22 21:08:22 +0300

    coockie sessions

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const sessionConfig = require('./session');
-
 
 const serverConfig = (app) => {
   app.use(morgan('dev'));
@@ -11,6 +11,15 @@ const serverConfig = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static('public'));
   app.use(cookieParser());
+  app.use(
+    cookieSession({
+      name: 'user_sid',
+      keys: process.env.SECRET,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 12,
+      },
+    })
+  );
   app.use(session(sessionConfig));
   app.disable('x-powered-by');
 };
