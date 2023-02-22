997345e15df60faf820348e0c94fc2b158842335
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-22 11:53:47 +0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const apiMail = require('./routes/mail.routes');
 const apiEmailForm = require('./routes/emailForm.routes');
 const apiTelegram = require('./routes/telegram.routes');
 const apiPhoneForm = require('./routes/phoneForm.routes');
+const apiAboutUs = require('./routes/description.routes');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -26,6 +27,7 @@ app.use('/api/mail', apiMail);
 app.use('/api/email', apiEmailForm);
 app.use('/api/telegram', apiTelegram);
 app.use('/api/phone', apiPhoneForm);
+app.use('/api/about', apiAboutUs);
 
 const start = async () => {
   try {
