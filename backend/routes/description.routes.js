2fc5cd637bb6dcd9fb4d830f0fd11c178dfb8191
eddy49reg <akhnazarov1990@gmail.com>
2023-02-23 11:54:48 +0300

    add idor check

--- backend/routes/description.routes.js
+++ backend/routes/description.routes.js
@@ -13,12 +13,14 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const { img, body } = req.body;
-    const newDescription = await Description.create({
-      img,
-      body,
-      userId: req.session.userId,
-    });
-    res.status(200).json(newDescription);
+    if (img !== '' && body !== '') {
+      const newDescription = await Description.create({
+        img,
+        body,
+        userId: req.session.userId,
+      });
+      res.status(200).json(newDescription);
+    }
   } catch (message) {
     res.status(500).json({ message: 'Crushed' });
   }
@@ -27,8 +29,13 @@ router.post('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    await Description.destroy({ where: { id } });
-    res.json(Number(id));
+    const idor = await Description.findOne({
+      where: { userId: req.session.userId },
+    });
+    if (req.session.userId === 1 || req.session.userId === idor.userId) {
+      await Description.destroy({ where: { id } });
+      res.json(Number(id));
+    }
   } catch ({ message }) {
     res.status(500).json(message);
   }
@@ -38,9 +45,16 @@ router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const { img, body } = req.body;
-    await Description.update({ img, body }, { where: { id } });
-    const data = await Description.findOne({ where: { id } });
-    res.json(data);
+    const idor = await Description.findOne({
+      where: { userId: req.session.userId },
+    });
+    if (img !== '' && body !== '') {
+      if (req.session.userId === 1 || req.session.userId === idor.userId) {
+        await Description.update({ img, body }, { where: { id } });
+        const data = await Description.findOne({ where: { id } });
+        res.json(data);
+      }
+    }
   } catch (message) {
     res.status(500).json({ message: ' Crushed' });
   }
